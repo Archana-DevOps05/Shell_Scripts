f4d9bf07fe9da086490aca1f152db1f71bc9bea7
Archana Singh <archusingh273@gmail.com>
2026-01-03 01:25:55 +0530

    This is a User Management Script

---
+++ user_management.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+########## User Account Management #########
+###### Author: Archana Singh
+
+
+action="$1"
+username="$2"
+
+case $action in 
+         "add")
+             useradd "$username"
+             ;;
+         "modify")
+             usermod -s /bin/bash "$username"
+             ;;
+         "delete")
+             userdel "$username"
+             ;;
+         *)
+            echo "Usage: $0 {add|modify|delete}  $username"
+            exit 1
+esac
